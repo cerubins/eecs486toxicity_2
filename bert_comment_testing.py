31609e3a1aa607dc6ef2f19122a47be87a9ad6ec
Ranya Liu <97542799+ranyal507@users.noreply.github.com>
2023-04-13 16:28:19 -0400

    Add files via upload

--- bert_comment_testing.py
+++ bert_comment_testing.py
@@ -13,8 +13,40 @@ from tensorflow.compat.v1 import InteractiveSession
 
 print("TESTING")
 
+# Loads finetuned BERT model and tokenizes with training tokenizer
 model = TFBertForSequenceClassification.from_pretrained("model")
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+
+# Shortens files for testing
+for file in glob.glob('comments_test_processed/*'):
+    with open(file) as f:    
+        lines = f.readlines()
+
+        filename = file.lstrip('comments_test_processed')
+        filename = filename.rstrip('.txt')
+        print(filename)
+
+        # Splits each subreddit's comments into multiple files of 250 lines
+        # eg: soccer_processed1.txt, soccer_processed2.txt, etc.....
+        count = 1
+        while(len(lines) > 250):
+            newLines = lines[:250]
+            lines = lines[250:]
+
+            # Outputs file of 250 comments
+            output = open('comments_test_processed_split/' + filename + str(count) + ".txt", 'w')
+            for line in newLines:
+                output.write(line)
+
+            count += 1
+        
+        # Outputs remaining comments
+        output = open('comments_test_processed_split/' + filename + str(count) + ".txt", 'w')
+        for line in lines:
+            output.write(line)
+
+# Loads trained BERT model from saved file
+model = TFBertForSequenceClassification.from_pretrained("model")
 
 # Iterates through subreddit comment files
 for file in sorted(glob.glob('thing/*')):
